9514f355a1ae11470570bc4981325962a3b97b68
vaibhav164 <vai1641996@gmail.com>
2026-04-22 06:24:35 +0530

    Updated File Structure

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import "react-native-reanimated";
+
+import { Text, View } from "react-native";
+
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
+
+export default function RootLayout() {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+}
